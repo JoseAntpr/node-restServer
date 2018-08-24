f17445627717e3b51f1315b8151d25a83976878d
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-24 14:00:00 +0200

    ADD: update user

--- server/routes/user.js
+++ server/routes/user.js
@@ -37,10 +37,22 @@ app.post('/user', (req, res) => {
 app.put('/user/:id', (req, res) => {
 
     let id = req.params.id;
-    res.json({
-        string: 'put usuario',
-        id
+    let body = req.body;
+
+    User.findByIdAndUpdate( id, body,{ new: true }, (err, userDB) => {
+        if( err ) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            }); 
+        }
+
+        res.status(200).json({
+            ok: true,
+            user: userDB
+        });
     });
+    
 });
 
 app.delete('/user', (req, res) => { 
