7bea8a63b41a25b8d5e53e8fac259170c8e7c936
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-08-24 21:22:02 +0200

    ADD: environments

---
+++ server/config/environments.js
@@ -0,0 +1,12 @@
+const prod = {
+    mongoURL: 'mongodb://joseantpr:123456jose@ds125322.mlab.com:25322/jose-cafe'
+};
+
+const dev = {
+    mongoURL: 'mongodb://localhost:27017/cafe'
+}
+
+module.exports = {
+    prod,
+    dev
+} 
