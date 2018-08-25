837d7e09a291aa92f2eec72ed25c318f25634663
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-08-25 16:23:12 +0200

    ADD: user login

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,9 +6,43 @@ const User = require('../models/user');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
-    })
+
+    let body = req.body;
+
+    User.findOne({ email: body.email }, (err, userDB) => {
+
+        if( err ) {
+            return res.status(500).json({
+                ok: false,
+                error: err
+            });
+        }
+
+        if( !userDB ) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: '(User) or password are incorrect'
+                }
+            });
+        }
+
+        if ( !bcrypt.compareSync( body.password,  userDB.password ) ) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: 'User or (password) are incorrect'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDB,
+            token: 123
+        })
+
+    });
 });
 
 module.exports = app;
