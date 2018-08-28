9ab91da304cd06e4c1b3a9b0f1e4f5290d0e544a
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-28 13:59:04 +0200

    ADD: create a default image & endpoint to show images

---
+++ server/routes/images.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.get('/image/:type/:img', (req, res) => {
+
+    let type = req.params.type;
+    let img = req.params.img;
+
+    let pathImg = `./uploads/${type}/${img}`;
+    let noImagePath = path.resolve(__dirname, '../assets/img/no-image.jpg');
+
+    res.sendFile(noImagePath);
+
+});
+
+module.exports = app;
